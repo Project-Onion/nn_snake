df0b6457064d59e6bafe22c2c45825445278734d
ohanoch <ohanoch@gmail.com>
2017-08-28 17:09:02 +0200

    fixed skipping bug - all is working

--- AA_snake_NN/json_parse.py
+++ AA_snake_NN/json_parse.py
@@ -8,28 +8,31 @@ NUMOFFILES=10
 trainMoves = []
 testMoves = []
 validMoves = []
-# allBoards = []
-trainBoards = []
-testBoards = []
-validBoards = []
+allBoards = []
+allMoves = []
+# trainBoards = []
+# testBoards = []
+# validBoards = []
 
 brushSuperApple = 6
 brushNormalApple = 5
-brushWinnerHead = 4
-brushWinnerBody = 3
 brushEnemyHead = 2
 brushEnemyBody = 1
+brushWinnerHead = 4
+brushWinnerBody = brushEnemyBody
+
+indexArray = []
 
 def findWinner(snakes):
-    max = 0;
-    maxIndex = 0;
+    max = 0
+    maxIndex = 0
     for snakeNum in range(len(snakes)):
         if int(snakes[snakeNum][0]) > max:
             max=int(snakes[snakeNum][0])
             maxIndex = snakeNum
     return maxIndex
 
-#def drawLine(start, end, isWinner, isInvisible, isHead):
+# def drawLine(start, end, isWinner, isInvisible, isHead):
 def drawLine(start, end, isWinner, isHead):
     brush = brushEnemyBody
     if isWinner:
@@ -55,7 +58,7 @@ def drawLine(start, end, isWinner, isHead):
                 board[i][start[1]]=brush
     if (isHead):
         if (isWinner):
-            board[start[0]][start[1]]=brushWinnerHead
+            board[start[0]][start[1]] = brushWinnerHead
         else:
             board[start[0]][start[1]] = brushEnemyHead
 
@@ -63,23 +66,24 @@ def drawSnakes(snakes):
     winnerNum=findWinner(snakes)
     for snakeNum in range(len(snakes)):
         if snakes[snakeNum][3]=="invisible":
-            break;
-#            isInvisible=True
-#        else:
-#            isInvisible=False
+            if snakeNum!=findWinner(snakes):
+                continue
+            isInvisible=True
+        else:
+            isInvisible=False
 
         if winnerNum==snakeNum:
             isWinner=True
         else:
             isWinner=False
 
-        for i in range(len(snakes[snakeNum])-6-1):#-2*isInvisible -1):
+        for i in range(len(snakes[snakeNum])-6-1-2*isInvisible): #notice, first coordinate is hwere snake took apple
             if (i==0):
                 isHead=True
             else:
                 isHead=False
-            drawLine(snakes[snakeNum][6+i],snakes[snakeNum][6+i+1], isWinner, isHead)
-#            drawLine(snakes[snakeNum][6+2*isInvisible+i],snakes[snakeNum][6+2*isInvisible+i+1], isWinner, isInvisible, isHead)
+            # drawLine(snakes[snakeNum][6+i],snakes[snakeNum][6+i+1], isWinner, isHead)
+            drawLine(snakes[snakeNum][6+2*isInvisible+i],snakes[snakeNum][6+2*isInvisible+i+1], isWinner, isHead)
 
 def printBoard():
     for i in range(BOARD_WIDTH):
@@ -97,38 +101,118 @@ def findMove(lastSnakes, currSnakes, lastWinner):
     else:
         isCurrInvisible = False
     
-    lastHead = tuple(map(int, lastSnakes[lastWinner][6+isCurrInvisible].split(',')))
-    currHead = tuple(map(int, currSnakes[lastWinner][6+isCurrInvisible].split(',')))
+    lastHead = tuple(map(int, lastSnakes[lastWinner][6+2*isLastInvisible].split(',')))
+    currHead = tuple(map(int, currSnakes[lastWinner][6+2*isCurrInvisible].split(',')))
 
-    if lastHead[0]==currHead[0]:
-        if lastHead[1] > currHead[1]:   #moved down
+    newLastHead = (lastHead[1], BOARD_HEIGHT - 1 - lastHead[0])
+    newCurrHead = (currHead[1], BOARD_HEIGHT - 1 - currHead[0])
+
+    firstKink = tuple(map(int, lastSnakes[lastWinner][7 + 2 * isLastInvisible].split(',')))
+    newfirstKink = (firstKink[1], BOARD_HEIGHT - 1 - firstKink[0])
+    if (newLastHead[0] < newfirstKink[0]):  # dir=left
+        if newLastHead[0] == newCurrHead[0]:    #is there no a x dimension turn
+            if newLastHead[1] > newCurrHead[1]:  # moved left
+                return 2
+            else:  # moved right
+                return 3
+        else:   #moved straight (continue in current x dimension)
             return 1
-        else:                           #moved up
-            return 0
+    elif (newLastHead[0] > newfirstKink[0]):  # dir=right
+        if newLastHead[0] == newCurrHead[0]:    #is there no a x dimension turn
+            if newLastHead[1] > newCurrHead[1]:  # moved right
+                return 3
+            else:  # moved left
+                return 2
+        else:   #moved straight (continue in current x dimension)
+            return 1
+
+    elif (newLastHead[1] < newfirstKink[1]):  # dir=down
+        if newLastHead[1] == newCurrHead[1]:    #is there no y dimension turn
+            if newLastHead[0] > newCurrHead[0]:  # moved right
+                return 3
+            else:  # moved left
+                return 2
+        else:   #moved straight (continue in current y dimension)
+            return 1
+    else:                                      # dir=up
+        if newLastHead[1] == newCurrHead[1]:    #is there no y dimension turn
+            if newLastHead[0] > newCurrHead[0]:  # moved left
+                return 2
+            else:  # moved right
+                return 3
+        else:   #moved straight (continue in current y dimension)
+            return 1
+
+
+    # if newLastHead[0]==newCurrHead[0]:
+    #     if newLastHead[1] > newCurrHead[1]:   #moved down
+    #         return 1
+    #     else:                           #moved up
+    #         return 0
+    # else:
+    #     if newLastHead[0] > newCurrHead[0]:   #moved left
+    #         return 2
+    #     else:                           #moved right
+    #         return 3
+
+def sphere (winningSnake, board):
+    if winningSnake[3]== "invisible":
+        isInvisible=True
     else:
-        if lastHead[0] > currHead[0]:   #moved left
-            return 2
-        else:                           #moved right
-            return 3
+        isInvisible = False
 
-def sphere (winningSnake):
-    if (winningSnake[6][0]<25):
-        board = np.roll(board,(25-winningSnake[6][0]),axis=1)
-    elif (winningSnake[6][0]>25):
-        board = np.roll(board, (50 - (winningSnake[6][0] - 25)), axis=1)
+    head = tuple(map(int, winningSnake[6 + 2*isInvisible].split(',')))
+    newHead = (head[1],BOARD_HEIGHT - 1 - head[0])
 
-    if (winningSnake[6][1]<25):
-        board = np.roll(board,(25-winningSnake[6][0]),axis=0)
-    elif (winningSnake[6][1]>25):
-        board = np.roll(board, (50 - (winningSnake[6][0] - 25)), axis=0)
+    if (newHead[1]<(BOARD_HEIGHT+1)/2):
+        board = np.roll(board,(newHead[1]+2+(BOARD_HEIGHT+1)/2),axis=0)
+    elif (newHead[1]>(BOARD_HEIGHT+1)/2):
+        board = np.roll(board, (newHead[1]-(BOARD_HEIGHT+1)/2), axis=0)
+
+    if (newHead[0]<(BOARD_WIDTH+1)/2):
+        board = np.roll(board,((BOARD_WIDTH+1)/2-newHead[0]),axis=1)
+    elif (newHead[0]>(BOARD_WIDTH+1)/2):
+        board = np.roll(board, (BOARD_WIDTH-newHead[0]+2+(BOARD_WIDTH+1)/2), axis=1)
+
+    firstKink = tuple(map(int, winningSnake[7 + 2*isInvisible].split(',')))
+    newfirstKink = (firstKink[1], BOARD_HEIGHT - 1 - firstKink[0])
+    if (newHead[0] < newfirstKink[0]): #dir=left
+        board = np.rot90(board,3)
+        board = np.roll(board, -1, axis=1)
+        board = np.roll(board, -3, axis=0)
+    elif (newHead[0] > newfirstKink[0]): #dir=right
+        board =  np.rot90(board, 1)
+        board = np.roll(board, -2, axis=0)
+    elif (newHead[1] < newfirstKink [1]): #dir=down
+        board = np.rot90(board, 2)
+        board = np.roll(board, -3, axis=0)
+    else:                               #dir=up
+        board = np.roll(board, -1, axis=1)
+        board = np.roll(board, -2, axis=0)
+
+    return board
 
 if __name__ == "__main__":
+    dataType = 0
     for fileNumber in range(0,NUMOFFILES):
+
+        #if fileNumber<6:
+        #    continue
+
         with open('./snake_json_files/'+ str(fileNumber) + '.json') as data_file:
             data = json.load(data_file)
 
+        reliantOnFlag = False
         for currState in range(0,len(data["states"])):
-            if data["states"][currState]["globalIndex"] > currGlobalIndex:
+            if data["states"][currState]["state"].split('\n')[0].split(' ')[0]=="Game":
+                continue
+
+            insertStateFlag = True  # fuckedUpFlag
+            if ((currState == (len(data["states"])-1)) or ((data["states"][currState]["globalIndex"]+1) != data["states"][currState+1]["globalIndex"])):
+                insertStateFlag = False;
+
+
+            if  int(data["states"][currState]["globalIndex"]) > currGlobalIndex:
                 board = np.array( [[0 for i in range(BOARD_WIDTH)] for j in range(BOARD_HEIGHT)])
                 currGlobalIndex = data["states"][currState]["globalIndex"]
                 splitData = data["states"][currState]["state"].split('\n')
@@ -141,59 +225,110 @@ if __name__ == "__main__":
 
                 snakes = [tuple(splitData[i].split(' ')) for i in range (2,6)]
 		
-		if (snakes[findWinner(snakes)][3]=='dead'):
-			break;
+                if (snakes[findWinner(snakes)][3]=='dead'):
+                    if reliantOnFlag:
+                        del allBoards[-1]
+                        del indexArray[-1]
+                    reliantOnFlag = False
+                    continue
+
+
                 drawSnakes(snakes)
 
-        np.pad(board, 1, 'constant', constant_values=(brushEnemyBody))  # add border to board for sphering
-        sphere(snakes[findWinner(snakes)])
+                # if dataType==0:
+                #     if reliantOnFlag: #currState != 0:
+                #         testMoves.append(findMove(lastSnakes,snakes, lastWinner))
+                #     if insertStateFlag:
+                #         board = np.pad(board, 1, 'constant', constant_values=(brushEnemyBody))  # add border to board for sphering
+                #         #board = sphere(snakes[findWinner(snakes)],board)
+                #         trainBoards.append(board)
+                #         reliantOnFlag = True
+                #     else:
+                #         reliantOnFlag = False
+                #     dataType=1
+                # elif dataType== 1:
+                #     if reliantOnFlag: #currState != 0:
+                #         trainMoves.append(findMove(lastSnakes,snakes, lastWinner))
+                #     if insertStateFlag:
+                #         board = np.pad(board, 1, 'constant', constant_values=(brushEnemyBody))  # add border to board for sphering
+                #         # board = sphere(snakes[findWinner(snakes)],board)
+                #         validBoards.append(board)
+                #         reliantOnFlag = True
+                #     else:
+                #         reliantOnFlag = False
+                #     dataType=2
+                # elif dataType== 2:
+                #     if reliantOnFlag: #currState != 0:
+                #         validMoves.append(findMove(lastSnakes,snakes, lastWinner))
+                #     if insertStateFlag:
+                #         board = np.pad(board, 1, 'constant', constant_values=(brushEnemyBody))  # add border to board for sphering
+                #         # board = sphere(snakes[findWinner(snakes)],board)
+                #         testBoards.append(board)
+                #         reliantOnFlag = True
+                #     else:
+                #         reliantOnFlag = False
+                #     dataType=0
 
-        if currGlobalIndex%3==0:
-            trainBoards.append(board)
-            if currState != 0:
-                trainMoves.append(findMove(lastSnakes,snakes, lastWinner))
-        elif currGlobalIndex % 3 == 1:
-            validBoards.append(board)
-            if currState != 0:
-                validMoves.append(findMove(lastSnakes,snakes, lastWinner))
-        elif currGlobalIndex % 3 == 2:
-            testBoards.append(board)
-            if currState != 0:
-                testMoves.append(findMove(lastSnakes,snakes, lastWinner))
-            # allBoards.append(board)
-            # if currState != 0:
-            #     moves.append(findMove(lastSnakes,snakes, lastWinner))
-                # board = board.transpose()
-                # allBoards.append(board)
                 # if currState != 0:
-                #     moves.append(findMove(lastSnakes,snakes, lastWinner))
+                #     allMoves.append(findMove(lastSnakes,snakes, lastWinner))
+                # board = np.pad(board, 1, 'constant',constant_values=(brushEnemyBody))  # add border to board for sphering
+                # # board = sphere(snakes[findWinner(snakes)], board)
+                # allBoards.append(board)
 
+                if reliantOnFlag:  # currState != 0:
+                    allMoves.append(findMove(lastSnakes,snakes, lastWinner))
+                if insertStateFlag:
+                    board = np.pad(board, 1, 'constant', constant_values=(brushEnemyBody))  # add border to board for sphering
+                    board = sphere(snakes[findWinner(snakes)],board)
+                    allBoards.append(board)
+                    reliantOnFlag = True
+                    indexArray.append((fileNumber,currGlobalIndex))
+                else:
+                    reliantOnFlag = False
+
+                    # board = board.transpose()
+                    # allBoards.append(board)
+                    # if currState != 0:
+                    #     allMoves.append(findMove(lastSnakes,snakes, lastWinner))
 
                 lastSnakes=snakes
-                lastWinner = findWinner(snakes);
+                lastWinner = findWinner(snakes)
 
-    numpyTrainBoard = np.array(trainBoards)
+    numpyAllBoard = np.array(allBoards)
     # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/trainingDataBoards', numpyTrainBoard)
+    np.save('./snake_json_files/allDataBoards', numpyAllBoard)
 
-    numpyValidBoard = np.array(validBoards)
+    numpyAllMoves = np.array(allMoves)
     # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/validationDataBoards', numpyValidBoard)
+    np.save('./snake_json_files/allDataMoves', numpyAllMoves)
 
-    numpyTestBoard = np.array(testBoards)
+    numpyIndexArray = np.array(indexArray)
     # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/testDataBoards', numpyTestBoard)
+    np.save('./snake_json_files/indexArray', numpyIndexArray)
 
-    numpyTrainMoves = np.array(moves)
-    # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/trainingDataMoves', numpyTrainMoves)
 
-    numpyValidMoves = np.array(moves)
-    # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/validationDataMoves', numpyValidMoves)
+    # numpyTrainBoard = np.array(trainBoards)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/trainingDataBoards', numpyTrainBoard)
+    #
+    # numpyValidBoard = np.array(validBoards)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/validationDataBoards', numpyValidBoard)
+    #
+    # numpyTestBoard = np.array(testBoards)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/testDataBoards', numpyTestBoard)
+    #
+    # numpyTrainMoves = np.array(trainMoves)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/trainingDataMoves', numpyTrainMoves)
+    #
+    # numpyValidMoves = np.array(validMoves)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/validationDataMoves', numpyValidMoves)
+    #
+    # numpyTestMoves = np.array(testMoves)
+    # # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
+    # np.save('./snake_json_files/testDataMoves', numpyTestMoves)
 
-    numpyTestMoves = np.array(moves)
-    # np.save('./snake_json_files/'+ str(fileNumber) + '_parsed.npy',board,True,False)
-    np.save('./snake_json_files/testDataMoves', numpyTestMoves)
 
-1
--- AA_snake_NN/test_board.py
+++ AA_snake_NN/test_board.py
@@ -1,12 +1,25 @@
 import numpy as np
 
-def drawBoard(boards, num):
+# boards=numpy.load('./snake_json_files/trainingDataBoards.npy')
+
+def drawBoard(boards, move, indexArray, num):
+	row = 0
+	head=(-1,-1)
 	for i in boards[num]:
+		row = row + 1
+		column = 0
 		for j in i:
+			column = column + 1
 			if j == 0:
 				print(" "),
 			else:
 				print(j),
-	print("")
+				if j==4:
+					head=(column,50 - row)
+		print("")
+	print("-------------------------------------------------------------")
+	print("head: " + str(head))
+	print ("move: "+ str(move[num]))
+	print ("index: " + str(indexArray[num]))
 
 
